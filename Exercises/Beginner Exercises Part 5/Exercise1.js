f80e2ffdbf740402184225d35d4abde2bc8ff666
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-06-21 00:54:15 -0700

    Beginner Exercises : Part 5

---
+++ Exercises/Beginner Exercises Part 5/Exercise1.js
@@ -0,0 +1,7 @@
+// Write a JavaScript function that returns a passed string with letters in alphabetical order.
+
+// Example string: 'webmaster'
+// Expected Output: 'abeemrstw'
+
+
+console.log('<====================> Exercise 1 <====================>');
