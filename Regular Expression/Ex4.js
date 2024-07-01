f2efe5f76bcdf1a4a00155c2e52883481847bd46
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-06-30 22:56:54 -0700

    Regular Expression

--- Regular Expression/Ex4.js
+++ Regular Expression/Ex4.js
@@ -1,2 +1,7 @@
-console.log('<====================> Exercise 2 <====================>');
+console.log('<====================> Exercise 4 <====================>');
 
+const text = "What is that? and h    t why it is hot. h.t h t"
+
+const regex1 = /h.t/g
+
+console.log(text.match(regex1));    // ['hat', 'hat', 'hot', 'h.t', 'h t']
