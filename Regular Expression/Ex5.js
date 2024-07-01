5029d6dc53a80bacaee008dc94661962f149f32b
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-01 07:25:09 -0700

    Regular Expression : Matchin Control Characters

--- Regular Expression/Ex5.js
+++ Regular Expression/Ex5.js
@@ -1,13 +1,17 @@
 console.log('<====================> Exercise 5 <====================>');
 
-const text = `h t h    t h
+const text = `hot hit h    t h
 t`
 
-const regex1 = /h.t/g
+const regex1 = /h.t/g;
 
-console.log(text.match(regex1));    // ['h t'], not matching the tab and new line
+console.log(text.match(regex1));    // ['hot', 'hit'], not matching the tab and new line
 
 
-const regex2 = /h\tt/g
+const regex2 = /h\tt/g;
+const regex3 = /h\nt/g;
+const regex4 = /h\r\nt/g;
 
-console.log(text.match(regex2));
+console.log(text.match(regex2));    // null
+console.log(text.match(regex3));    // ['h\nt']
+console.log(text.match(regex4));    // null
