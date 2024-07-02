0af2ab7e2ee272a1bb82c9eea7539a90805426d9
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-02 05:19:23 -0700

    RegEx : Lookahead Groups

--- Regular Expression/Ex21.js
+++ Regular Expression/Ex21.js
@@ -2,3 +2,13 @@ console.log('<====================> Exercise 21 <====================>');
 
 // Using Lookahead Groups in the RegEx Pattern
 
+const text = `google.com facebook.com`;
+
+const regex1 = /\w+(\.com)/g;
+const regex2 = /\w+(?=\.com)/g;
+
+console.log(text.match(regex1));    // ['google.com', 'facebook.com']
+console.log(text.match(regex2));    // ['google', 'facebook']
+
+
+
