60cdcfc1f6781705e974452fc212c3f9169eeb87
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-08 04:28:29 -0700

    RegEx

--- Regular Expression/Ex29.js
+++ Regular Expression/Ex29.js
@@ -3,7 +3,6 @@ console.log('<====================> Exercise 29 <====================>');
 // Validating the Data Format using RegEx.
 
 const date = '08/07/2024';
+const regex = /^(3[01]|[12][0-9]|0?[1-9])\/(1[0-2]|0?[1-9])\/(([0-9]{2})?[0-9]{2})$/;
 
-const regex = /^(3[01]|[1-2][0-9]|0?[1-9])/(1[0-2]|0?[1-9])/(([0-9]{2})?[0-9]{2})$/g
-
-console.log(date.match(exec));
+console.log(date.match(regex));
