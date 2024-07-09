d99626a860a63b9351df43a90d4a7d11a7a2074c
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-09 05:15:30 -0700

    Promises

--- Exercises/Promises/script3.js
+++ Exercises/Promises/script3.js
@@ -2,5 +2,11 @@ const request = fetch('https://api.github.com/users/codebubb');
 
 request
     .then((result) => {
-        
+        return result.json();
+    })
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
     })
